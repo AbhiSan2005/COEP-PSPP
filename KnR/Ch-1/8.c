067fccd87092254044569e3f8b23ebb8c6239d3f
Abhiraj Sankpal <sankpalabhiraj1@gmail.com>
2024-10-26 13:28:00 +0530

    Update 8.c

--- KnR/Ch-1/8.c
+++ KnR/Ch-1/8.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() {
+  int ch, bl=0, nl=1, tab=0;
+  while ((ch = getchar()) != EOF) {
+    if (ch == ' ') {
+      ++bl;
+    }
+    if (ch == '\n') {
+      ++nl;
+    }
+    if (ch == '\t') {
+      ++tab;
+    }
+    if (ch == '\0') {
+      break;
+    }
+  }
+  printf("\nblanks : %d\n", bl);
+  printf("tabs: %d\n", tab);
+  printf("lines : %d\n", nl);
+  return 0;
+}
