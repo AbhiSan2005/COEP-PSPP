40b49f5fd50d7cb48b6eba67bf20a70cda6de510
Abhiraj Sankpal <sankpalabhiraj1@gmail.com>
2024-10-26 14:41:10 +0530

    Add files via upload

---
+++ KnR/Ch-1/9.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int c, lastc;
+    lastc = ' ';
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            if (lastc != ' ')
+                putchar(c);
+        }
+	else {
+            putchar(c);
+	}
+	lastc = c;
+    }
+    return 0;
+}
