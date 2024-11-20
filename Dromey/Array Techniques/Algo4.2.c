48b6c641dc01168c1e0fdc302cd0d4cb5522d15d
Abhiraj Sankpal <sankpalabhiraj1@gmail.com>
2024-11-20 11:01:40 +0530

    Update Algo4.2.c

--- Dromey/Array Techniques/Algo4.2.c
+++ Dromey/Array Techniques/Algo4.2.c
@@ -4,8 +4,7 @@ int main(void)
 {
 	int n, b; //MARKS ARE BETWEEN 0 AND N(INCLUDING)
 	scanf("%d", &n);
-	int a[n+2];
-	a[n+1] = '\0';
+	int a[n+1];
 	for (int i = 0; i <= n; ++i)
 		a[i] = 0;
 	while (scanf("%d", &b) != EOF && b <= n)
